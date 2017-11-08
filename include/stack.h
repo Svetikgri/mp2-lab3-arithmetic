a7e2ab928798e7591e4d3533daf6cfd81f3fea4c
Svetikgri <Svetik_gri@inbox.ru>
2017-11-08 23:47:05 +0400

    OKK

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,27 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+#ifndef _Stack_h_
+#define _Stack_h_
+
+#define Size 25
+
+template <class ValType>
+class Stack
+{
+protected:
+  ValType *pstack;// массив для стека
+  int pos;//индекс последнего занятого
+  
+public:
+	Stack()
+	{
+		pstack=new ValType[Size];
+	}
+	~Stack();
+	 void Put(const  ValType n);  //положить
+	 void Take(const  ValType n);  //извлечь элемент
+	 int Look(const  ValType n);  
+	 int Check();
+	 int Get();//получить кол-во эл-тов
+};
